ef06534f6d9442af56015ef21f6888df42687f50
Cephas Mwai <cephasmwai561@gmail.com>
2025-03-30 10:54:47 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(record) {
+   
+    const winningYear = record.find(game => game.result === "W");
+    
+    
+    return winningYear ? winningYear.year : undefined;
+}
+
+
+module.exports = superbowlWin;
